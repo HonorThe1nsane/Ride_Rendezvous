dbe1fcea555b251111b5bcc34caf62bbfda67b01
ZaidaGrilli <grillizaida@gmail.com>
2023-07-01 00:01:42 -0400

    car controllers added but i need ip adress?

--- controllers/cars.js
+++ controllers/cars.js
@@ -4,7 +4,7 @@
 const mongodb = require('../db/connect');
 
 const db = require('../models');
-const Car = db.cars;
+const carSchema = db.cars;
 
 const getAllCars = async (req, res) => {
     const result = await mongodb.getDb().db('Ride_Rendezvous').collection('cars').find();
@@ -16,4 +16,46 @@ const getAllCars = async (req, res) => {
 
 };
 
-module.exports = { getAllCars };
+const postCar = async (req, res) => {
+    try {
+        const cars = carSchema({
+            carMake: req.body.carMake,
+            carModel: req.body.carModel,
+            engineSize: req.body.engineSize,
+            color: req.body.color,
+            year: req.body.year,
+            price: req.body.price,
+        });
+        const data = await cars.save();
+        res.json(data);
+    } catch {
+        res.status(400).json({ message: err });
+    }
+};
+
+const updateCar = async (req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+    try {
+        const data = await carSchema.updateOne({ _id: id }, { $set: body });
+
+        if (data.modifiedCount == 0) {
+            res.status(200).json({ message: "car doesn't exist", carData: data });
+            return;
+        }
+        res.status(200).json({ message: "car updated", carData: data });
+    } catch (err) {
+        res.status(400).json({ message: err });
+    }
+};
+
+const deleteCar = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const data = await carSchema.deleteOne({ _id: id });
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(400).json({ message: err });
+    }
+};
+module.exports = { getAllCars, postCar, updateCar, deleteCar};
