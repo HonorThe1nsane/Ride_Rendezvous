7ca353b997b93176d963da256e56d0f320043971
HonorThe1nsane <troymd2009@hotmail.com>
2023-07-03 15:14:54 -0600

    Updated events controller

---
+++ routes/events.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const eventsController = require('../controllers/events');
+const { check, validationResult } = require('express-validator');
+
+
+router.get('/', eventsController.getAllEvents);
+
+router.get('/:id', eventsController.getSingleEvent);
+
+router.post('/', [
+    check('eventName').not().trim().isEmpty().escape(),
+    check('date').not().trim().isEmpty().escape(),
+    check('time').not().trim().isEmpty().escape(),
+    check('').not().trim().isEmpty().escape(),
+    check('').not().trim().isEmpty().escape(),
+], eventsController.createEvent);
+
+
+
+module.exports = router;
