20e4f4fdc5e38e2cc017e50c6f5613c81b9740e4
Zamza584 <mzamalloa97@gmail.com>
2023-07-03 23:58:52 -0500

    added validators

--- routes/person.js
+++ routes/person.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
 const personController = require("../controllers/person");
+const { personsValidationRules, validate } = require("../utils/validator");
 
 router.get("/", personController.getAll);
-router.post("/", personController.postPerson);
-router.put("/:id", personController.updatePerson);
+router.get("/:id", personController.getSpecific);
+router.post("/", personsValidationRules(), validate, personController.postPerson);
+router.put("/:id", personsValidationRules(), validate, personController.updatePerson);
 router.delete("/:id", personController.deletePerson);
 
 module.exports = router;
