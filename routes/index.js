d5fa218748c3cd6be1006a873e2c7b3514c781a1
HonorThe1nsane <troymd2009@hotmail.com>
2023-07-19 16:58:29 -0600

    udpated redirect

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ const router = express.Router();
 router.get('/home', (req, res) => {
     res.send('This is our 341 team project.');
 });
-
+router.post('/login/callback', (req, res) => {
+    res.redirect('https://riderendezvous.onrender.com/api-docs');
+});
 router.use('/person', require('./person'));
 
 router.use('/cars', require('./cars'));
